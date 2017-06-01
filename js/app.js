c5d2e2f380fe74c4e43cbb80db34e105d7811fd7
Tim Acker <tjacker31@gmail.com>
2017-06-01 01:25:49 -0400

    ScrollReveal
    Added ScrollReveal library and code to initialize the plugin.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+/*jslint esversion: 6, browser: true*/
+/*global window, console, $, jQuery, alert*/
+
 $(document).foundation();
+
+window.sr = ScrollReveal(
+  {
+//    origin: 'top',
+    duration: 900,
+    delay: 100,
+    rotate: {
+      x: 0, y: 0, z: 0
+    },
+    opacity: 0,
+    scale: 1
+  }
+);
+sr.reveal('.sr-links', 300);
