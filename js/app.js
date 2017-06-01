a5bf8314b7d8ffe0c73173f7090649ade708d77e
Tim Acker <tjacker31@gmail.com>
2017-06-01 17:18:46 -0400

    Navigation and Callout
    Added transitions for the navigation menu links and callout section.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 /*jslint esversion: 6, browser: true*/
-/*global window, console, $, jQuery, alert*/
+/*global window, console, $, jQuery, ScrollReveal, sr, alert*/
 
 $(document).foundation();
 
-window.sr = ScrollReveal(
-  {
-//    origin: 'top',
-    duration: 900,
-    delay: 100,
-    rotate: {
-      x: 0, y: 0, z: 0
-    },
-    opacity: 0,
-    scale: 1
-  }
-);
-sr.reveal('.sr-links', 300);
+// Default overrides for all ScrollReveal elements
+const defaults = {
+  origin: 'left',
+  duration: 300,
+  delay: 500,
+  distance: '600px',
+  scale: 1
+};
+
+const defaultInterval = 300;
+
+const srLinks = {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '40px'
+};
+
+const srNameTitle = {
+  origin: 'left',
+  duration: 500,
+  distance: '40px',
+  delay: 0
+};
+
+window.sr = ScrollReveal(defaults);
+sr.reveal('.sr-links', srLinks, defaultInterval);
+sr.reveal('.sr-name-title', srNameTitle, 0);
