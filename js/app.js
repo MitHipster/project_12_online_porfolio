ff04a4feadd7f42660019b3cfcdd7b43a99852ca
Tim Acker <tjacker31@gmail.com>
2017-06-02 13:19:01 -0400

    Connect
    Added transitions for the connect section and made final ScrollReveal adjustments to entire portfolio.

--- js/app.js
+++ js/app.js
@@ -3,41 +3,68 @@
 
 $(document).foundation();
 
+
+
 // Default overrides for all ScrollReveal elements
 const defaults = {
   origin: 'left',
-  duration: 750,
-  delay: 500,
-  distance: '100px'
-};
-
-const defaultInterval = 300;
-
-const srLink = {
-  origin: 'bottom',
   duration: 1000,
-  distance: '40px',
+  delay: 300,
+  distance: '30%',
+  viewFactor: 0.6,
   viewOffset: {
-    bottom: 50
+    top: 50
   }
 };
 
+// Default interval for nav links
+const defaultInterval = 300;
+
+// Nav Link options
+const srLink = {
+  container: '.sticky',
+  origin: 'top',
+  duration: 1000,
+  scale: 1,
+  viewOffset: {
+    top: 0
+  }
+};
+
+// Callout options
 const srNameTitle = {
   duration: 600,
   delay: 0
 };
 
+// Portfolio image options
 const srPortImg = {
   origin: 'bottom'
 };
 
+// Portfolio descriptions options for right side layout. Left side layout uses default settings
 const srPortDescRight = {
   origin: 'right'
 };
 
+// Get In Touch options
+const srGetInTouch = {
+  origin: 'bottom'
+};
+
+// Contact Info options. Message form uses default options
+const srContact = {
+  origin: 'right'
+};
+
+// Initialize ScrollReveal
 window.sr = ScrollReveal(defaults);
+// Call ScrollReveal passing container and custom / default options
 sr.reveal('.sr-link', srLink, defaultInterval);
 sr.reveal('.sr-name-title', srNameTitle);
 sr.reveal('.sr-port-img', srPortImg);
 sr.reveal('.sr-port-desc-r', srPortDescRight);
 sr.reveal('.sr-port-desc-l');
+sr.reveal('.sr-get-in-touch', srGetInTouch);
+sr.reveal('.sr-form');
+sr.reveal('.sr-contact', srContact);
