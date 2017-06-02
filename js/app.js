3ecb78fd8973e163eebceaafbc4b153a02d1bb76
Tim Acker <tjacker31@gmail.com>
2017-06-02 00:41:24 -0400

    Porfolio
    Added transitions for the portfolio section.

--- js/app.js
+++ js/app.js
@@ -6,27 +6,38 @@ $(document).foundation();
 // Default overrides for all ScrollReveal elements
 const defaults = {
   origin: 'left',
-  duration: 300,
+  duration: 750,
   delay: 500,
-  distance: '600px',
-  scale: 1
+  distance: '100px'
 };
 
 const defaultInterval = 300;
 
-const srLinks = {
+const srLink = {
   origin: 'bottom',
   duration: 1000,
-  distance: '40px'
+  distance: '40px',
+  viewOffset: {
+    bottom: 50
+  }
 };
 
 const srNameTitle = {
-  origin: 'left',
-  duration: 500,
-  distance: '40px',
+  duration: 600,
   delay: 0
 };
 
+const srPortImg = {
+  origin: 'bottom'
+};
+
+const srPortDescRight = {
+  origin: 'right'
+};
+
 window.sr = ScrollReveal(defaults);
-sr.reveal('.sr-links', srLinks, defaultInterval);
-sr.reveal('.sr-name-title', srNameTitle, 0);
+sr.reveal('.sr-link', srLink, defaultInterval);
+sr.reveal('.sr-name-title', srNameTitle);
+sr.reveal('.sr-port-img', srPortImg);
+sr.reveal('.sr-port-desc-r', srPortDescRight);
+sr.reveal('.sr-port-desc-l');
